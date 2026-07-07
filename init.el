b9539140d41691767de01a613c66ba805387d520
AjaykrishnanR <theconsciousstoictaoistcat@gmail.com>
2026-07-07 08:56:29 +0530

    lsp

--- init.el
+++ init.el
@@ -69,10 +69,6 @@
     (setq explicit-pwsh-args '("-NoProfile" "-Interactive"))
     (setq explicit-pwsh.exe-args '("-NoProfile" "-Interactive"))))
 
-;; THEMES
-(load-theme `tango-dark)
-;;(load-theme `modus-vivendi) 
-
 ;; PACKAGES (WITH OR WITHOUT DOWNLODING)
 ;;;; custom.el: to save the state in a different file
 (setq custom-file (locate-user-emacs-file "custom.el"))
@@ -105,3 +101,57 @@
   :config
   (setq completion-styles `(orderless basic))
   (setq completion-category-defaults nil))
+
+;;;; COMPANY MODE
+;;;; Provides the UI for LSP autocomplete etc...
+(use-package company
+  :ensure t
+  :config
+  (company-mode 1))
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; MELPA PACKAGE MANAGER
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+;; LSP
+;; C# lsp server based on csharp-ls (https://github.com/razzmatazz/csharp-language-server)
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :hook (csharp-mode . lsp-deferred)
+  :config
+  (setq lsp-csharp-server-type 'csharp-ls))
+(use-package lsp-ui
+  :ensure t)
+
+;; THEMES
+;;;; INBUILT
+;;;; (load-theme `tango-dark)
+;;;; (load-theme `modus-vivendi)
+;;;; EXTERNAL
+;;;; EF-THEMES by Protesilaos
+(use-package ef-themes
+  :ensure t
+  :init
+  ;; This makes the Modus commands listed below consider only the Ef
+  ;; themes.  For an alternative that includes Modus and all
+  ;; derivative themes (like Ef), enable the
+  ;; `modus-themes-include-derivatives-mode' instead.  The manual of
+  ;; the Ef themes has a section that explains all the possibilities:
+  ;;
+  ;; - Evaluate `(info "(ef-themes) Working with other Modus themes or taking over Modus")'
+  ;; - Visit <https://protesilaos.com/emacs/ef-themes#h:6585235a-5219-4f78-9dd5-6a64d87d1b6e>
+  (ef-themes-take-over-modus-themes-mode 1)
+  :bind
+  (("<f5>" . modus-themes-rotate)
+   ("C-<f5>" . modus-themes-select)
+   ("M-<f5>" . modus-themes-load-random))
+  :config
+  ;; All customisations here.
+  (setq modus-themes-mixed-fonts t)
+  (setq modus-themes-italic-constructs t)
+
+  ;; Finally, load your theme of choice (or a random one with
+  ;; `modus-themes-load-random', `modus-themes-load-random-dark',
+  ;; `modus-themes-load-random-light').
+  (modus-themes-load-theme 'ef-cyprus))
